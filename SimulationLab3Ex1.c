551e5aa0609c7011d06fdd6561609554d47f8115
cbruni <carmen.bruni@gmail.com>
2025-01-31 10:57:12 -0500

    Update SimulationLab3Ex1.c

--- SimulationLab3Ex1.c
+++ SimulationLab3Ex1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-    
-int main(int argc, char **argv) {
-    char *items[] = {"boat", "car", "truck", "train"};
-    int index = atoi(argv[1]);
-    printf("You selected %s\n", items[index]);
-}
+//Not actually SimLab3Ex1...
+int main() {
+	char last_name[20];
+	printf ("Enter your last name: ");
+	scanf ("%s", last_name);
+	printf("Entered last name is %s\n", last_name);
+	}
